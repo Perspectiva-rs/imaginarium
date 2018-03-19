cd3a64ed095f033a94958e08c4df18541e58f736
José Filipe <jn_filipe@hotmail.com>
2018-03-19 17:51:05 +0000

    Secound Commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,21 @@
 extern crate compositio;
 
+use std::ops::Index;
+use compositio::OwnedMatrix;
+
+// type Image<T> = compositio::Matrix<T,Vec<T>,Vec<usize>>;
+struct Image<T>(OwnedMatrix<T>);
+
+impl<'a,T> Index<&'a [usize]> for Image<T>{
+    type Output = T;    
+    fn index(& self, index:&[usize]) -> &T{
+        &self.0[index]
+    }
+}
+
+
+
+
 #[cfg(test)]
 mod tests {
     #[test]
